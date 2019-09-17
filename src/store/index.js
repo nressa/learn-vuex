1d233b31e1f31a321ec49089901eeea52af7d72a
Ressa Magbanua <mressanova@gmail.com>
2019-09-17 21:51:10 +0800

    update index.js same codes

--- src/store/index.js
+++ src/store/index.js
@@ -63,16 +63,16 @@ export default new Vuex.Store({
 			})
 		},
 
-		addProductToCart(context, product){
-			if(product.inventory > 0){
-				const cartItem = context.state.cart.find(item => item.id === product.id)
+		addProductToCart({state, getters, commit}, product){
+			if(getters.productIsInStock(product)){
+				const cartItem = state.cart.find(item => item.id === product.id)
 				if(!cartItem){
-					context.commit('pushProductToCart', product.id)
+					commit('pushProductToCart', product.id)
 				} else{
-					context.commit('incrementItemQuantity', cartItem)
+					commit('incrementItemQuantity', cartItem)
 				}
 
-				context.commit('decrementProductInventory', product)
+				commit('decrementProductInventory', product)
 			}
 		},
 
